ca88eb374d8c0267641e6389d707e4db501cb516
chrscmpl <chcampolongo1@gmail.com>
2022-08-29 13:05:12 +0200

    Add Library logic

--- index.js
+++ index.js
@@ -14,3 +14,67 @@ Book.prototype.info = function () {
 		this.read ? 'read' : 'not read yet',
 	];
 };
+
+Book.prototype.card = function () {
+	const card = document.createElement('div');
+	card.classList.add('card');
+	this.info().forEach(info => {
+		const field = document.createElement('div');
+		field.textContent = info;
+		card.appendChild(field);
+	});
+	return card;
+};
+
+function Library(grid, blacklist, cards) {
+	this.grid = grid;
+	this.blacklist = initArray(blacklist);
+	this.cards = initArray(cards);
+}
+
+Library.prototype.push = function (card) {
+	if (!(card instanceof HTMLElement))
+		throw "Can't add non HTML Elements to library";
+	this.cards.unshift(card);
+	this.update();
+};
+
+Library.prototype.remove = function (card) {
+	this.cards = this.cards.filter(elem => elem !== card);
+	this.update();
+};
+
+Library.prototype.update = function () {
+	for (card of this.grid.children) card.remove();
+	for (card of this.cards) this.grid.appendChild(card);
+	for (blacklisted of this.blacklist) this.grid.appendChild(blacklisted);
+};
+
+//returns a one dimensional array no matter if the argument is undefined, an array, or a single element
+function initArray(arg) {
+	return !arg
+		? []
+		: typeof arg[Symbol.iterator] === 'function' //is iterable
+		? [...arg]
+		: [arg];
+}
+
+document.querySelector('.add-card').addEventListener(
+	'click',
+	function () {
+		this.classList.add('shown');
+	}.bind(document.querySelector('.popup-container'))
+);
+
+document.querySelector('.popup-container').addEventListener(
+	'click',
+	function (e) {
+		if (e.target !== this) return;
+		this.classList.remove('shown');
+	}.bind(document.querySelector('.popup-container'))
+);
+
+const library = new Library(
+	document.querySelector('.books-grid'),
+	document.querySelector('.add-card')
+);
